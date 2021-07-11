0101389072d635295e8d3f844aa0d7e5dae8f099
Helber Fabián Toro <helber.toro@gmail.com>
2021-07-10 22:19:17 -0500

    Simple web with create react app

---
+++ src/containers/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Home = (props) => {
+  return (
+    <p>Home</p>
+  );
+};
+
+Home.propTypes = {
+
+};
+
+Home.defaultProps = {
+
+};
---
+++ src/routes/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { Home } from '../containers/Home';
+import { NotFound } from '../containers/NotFound';
+
+const App = () => (
+  <BrowserRouter basename="/">
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
