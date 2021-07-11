e3965663031da068638d5399b5b3c5d35b665d19
Helber Fabián Toro <helber.toro@gmail.com>
2021-07-11 00:17:07 -0500

    Top component

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Logo from '../../statics/images/MFS.png';
 import './Top.sass';
 
 export const Top = (props) => {
   return (
-    <p>Top</p>
+    <div className="Top">
+      <div className="Top-wrapper">
+        <figure>
+          <img src={Logo} alt="Motion Factory Studios" />
+        </figure>
+        <nav>
+          <ul>
+            <li><a href="?">Home</a></li>
+            <li><a href="?">Nosotros</a></li>
+            <li><a href="?">Servicios</a></li>
+            <li><a href="?">Portafolio</a></li>
+            <li><a href="?">Blog</a></li>
+            <li><a href="?">Contacto</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
