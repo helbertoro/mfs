1879f56689516c984a0eeff7eb1da469355e9bcb
Helber Fabián Toro <helber.toro@gmail.com>
2021-07-14 21:38:39 -0500

    Footer contact form styles

--- src/components/Footer/ContactForm.jsx
+++ src/components/Footer/ContactForm.jsx
@@ -3,9 +3,24 @@ import PropTypes from 'prop-types';
 import './ContactForm.sass';
 
 export const ContactForm = (props) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submited');
+  };
+
   return (
     <div className="ContactForm">
-      <p>Contact form</p>
+      <form onSubmit={event => handleSubmit(event)}>
+        <input className="ContactForm-element" type="text" name="name" placeholder="Nombre*"/>
+        <input className="ContactForm-element" type="text" name="email" placeholder="Correo electrónico*"/>
+        <textarea className="ContactForm-element" name="Message" placeholder="Mensaje" cols="30" rows="4"></textarea>
+        <button
+          type="submit"
+          className="ContactForm-button"
+        >
+          enviar
+        </button>
+      </form>
     </div>
   );
 };
