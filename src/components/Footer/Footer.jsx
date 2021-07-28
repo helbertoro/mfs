2ae10494ce3c93cde7d2ce393e4938b946fda6d8
Helber Fabián Toro <helber.toro@gmail.com>
2021-07-27 20:28:34 -0500

    Social networks

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhoneAlt, faMobileAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { ContactForm } from './ContactForm.jsx';
 import { Social } from './Social.jsx';
 import './Footer.sass';
@@ -12,21 +14,22 @@ export const Footer = (props) => {
           <h5>¡Estás a un clic de iniciar una experiencia que revolucionará tu marca!</h5>
           <ul>
             <li>
-              icon
+              <FontAwesomeIcon icon={faPhoneAlt}/>
               <span>PBX: 571- 472 5933</span>
             </li>
             <li>
-              icon
+              <FontAwesomeIcon icon={faMobileAlt}/>
               <span>Móvil: (57) 310 785 1233</span>
             </li>
             <li>
-              icon
+              <FontAwesomeIcon icon={faEnvelope}/>
               <span>info@motionfactorystudios.com</span>
             </li>
           </ul>
         </div>
         <div className="Footer-form">
           <ContactForm/>
+          <p>Bogotá - Colombia</p>
         </div>
         <div className="Footer-social">
           <Social/>
--- src/components/Footer/Social.jsx
+++ src/components/Footer/Social.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faInstagram, faYoutube, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import './Social.sass';
 
 export const Social = (props) => {
   return (
     <div className="Social">
-      Social
+      <ul>
+        <li>
+          <a href="?">
+            <FontAwesomeIcon icon={faFacebookF}/>
+          </a>
+        </li>
+        <li>
+          <a href="?">
+            <FontAwesomeIcon icon={faInstagram}/>
+          </a>
+        </li>
+        <li>
+          <a href="?">
+            <FontAwesomeIcon icon={faYoutube}/>
+          </a>
+        </li>
+        <li>
+          <a href="?">
+            <FontAwesomeIcon icon={faLinkedinIn}/>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
