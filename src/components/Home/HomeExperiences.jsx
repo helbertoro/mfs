1df68067e2d8822e1a0fa8ab6eee6159fe797c29
Helber Fabián Toro <helber.toro@gmail.com>
2021-08-04 22:31:27 -0500

    HomeExperiences component to array

--- src/components/Home/HomeExperiences.jsx
+++ src/components/Home/HomeExperiences.jsx
@@ -5,30 +5,38 @@ import { faQuoteRight, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 import './HomeExperiences.sass';
 
 export const HomeExperiences = (props) => {
+  const data = [
+    {
+      id: 1,
+      content: 'Lorem ipsum dolor sit amet consectetur adipiscing, elit hendrerit vel donec senectus',
+      author: 'Dra. Luisa Plata',
+    },
+    {
+      id: 2,
+      content: 'Suscipit nunc porttitor tincidunt tempor ullamcorper phasellus nullam habitant',
+      author: 'Sabana Race',
+    },
+    {
+      id: 3,
+      content: 'Venenatis semper quisque tellus montes ante a parturient non',
+      author: 'Dr. Raul Daza',
+    },
+  ];
+
   return (
     <div className="HomeExperiences">
       <div className="HomeExperiences-title">
         <p><b>Experiencias reales</b> <br /> que nos inspiran</p>
       </div>
       <div className="HomeExperiences-testimonials">
-        <div className="HomeExperiences-testimonials-item">
-          <FontAwesomeIcon className="HomeExperiences-quote-left" icon={faQuoteLeft}/>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing, elit hendrerit vel donec senectus</p>
-          <p><b>Dra. Luisa Plata</b></p>
-          <FontAwesomeIcon className="HomeExperiences-quote-right" icon={faQuoteRight}/>
-        </div>
-        <div className="HomeExperiences-testimonials-item">
-          <FontAwesomeIcon className="HomeExperiences-quote-left" icon={faQuoteLeft}/>
-          <p>Suscipit nunc porttitor tincidunt tempor ullamcorper phasellus nullam habitant</p>
-          <p><b>Sabana Race</b></p>
-          <FontAwesomeIcon className="HomeExperiences-quote-right" icon={faQuoteRight}/>
-        </div>
-        <div className="HomeExperiences-testimonials-item">
-          <FontAwesomeIcon className="HomeExperiences-quote-left" icon={faQuoteLeft}/>
-          <p>Venenatis semper quisque tellus montes ante a parturient non</p>
-          <p><b>Dr. Raul Daza</b></p>
-          <FontAwesomeIcon className="HomeExperiences-quote-right" icon={faQuoteRight}/>
-        </div>
+        {data.map((item) => (
+          <div key={`HomeExperiences-testimonials-${item.id}`} className="HomeExperiences-testimonials-item">
+            <FontAwesomeIcon className="HomeExperiences-quote-left" icon={faQuoteLeft}/>
+            <p>{item.content}</p>
+            <p><b>{item.author}</b></p>
+            <FontAwesomeIcon className="HomeExperiences-quote-right" icon={faQuoteRight}/>
+          </div>
+        ))}
       </div>
     </div>
   );
