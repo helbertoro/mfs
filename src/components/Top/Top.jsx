d1bc516e84ca955fcbca3230bc612f91f4eefb43
Helber Fabián Toro <helber.toro@gmail.com>
2021-08-06 08:54:22 -0500

    Submenu

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -14,7 +14,17 @@ export const Top = (props) => {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/nosotros">Nosotros</a></li>
-            <li><a href="?">Servicios</a></li>
+            <li><span>Servicios</span>
+              <ul className="Top-submenu">
+                <li><a href="?">Producción de video</a></li>
+                <li><a href="?">Transmisiones de video en vivo</a></li>
+                <li><a href="?">Marketing digital</a></li>
+                <li><a href="?">Sitios web</a></li>
+                <li><a href="?">Diseño gráfico</a></li>
+                <li><a href="?">Fotografía profesional</a></li>
+                <li><a href="?">Eventos corporativos</a></li>
+              </ul>
+            </li>
             <li><a href="/portafolio">Portafolio</a></li>
             <li><a href="?">Blog</a></li>
             <li><a href="?">Contacto</a></li>
